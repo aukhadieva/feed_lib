28942c803173297f84f433fcaefd1d8af940beb8
Olya <olya.aukhadieva@gmail.com>
2026-03-17 10:26:24 +0500

    initial

---
+++ feed_lib/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, mapped_column
+from uuid import UUID, uuid4
+
+BaseDBModel = declarative_base()
+
+class TestModelDB(BaseDBModel):
+    """Test database model."""
+    __tablename__ = 'test_model'
+
+    uuid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4())
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
